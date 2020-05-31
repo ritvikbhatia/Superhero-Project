3ea9f99c91f0f308bf0cb6ef9c5ebab96bfdf462
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-31 14:48:32 +0530

    add api

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
+document.getElementById("myform").addEventListener("submit", myFunction);
 
-var xhrRequest= new XMLHttpRequest();
+function myFunction() {
+    var xhrRequest= new XMLHttpRequest();
 
-xhrRequest.onload=function()
-{
-    console.log(xhrRequest.response);
+    xhrRequest.onload=function()
+    {
+        console.log(xhrRequest.response);
+        var responseJSON=JSON.parse(xhrRequest.response);
+        alert(responseJSON.name);
+        
+    }
+    var superHeroName=document.getElementById("superHeroName").value;
+    xhrRequest.open('get',"https://superheroapi.com/api/4630807856945450/search/"+superHeroName,false);
+    xhrRequest.send();
     
 }
-xhrRequest.open('get',"https://superheroapi.com/api/4630807856945450/644",true);
-xhrRequest.send();
+
+
