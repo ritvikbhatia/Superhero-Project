252b0f88fb505675e7fd8a85d4b0f0f07ada816d
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-31 15:20:55 +0530

    add links

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+var names=[];
 document.getElementById("myform").addEventListener("submit", myFunction);
 
 function myFunction() {
@@ -7,12 +8,22 @@ function myFunction() {
     {
         console.log(xhrRequest.response);
         var responseJSON=JSON.parse(xhrRequest.response);
-        alert(responseJSON.name);
+        for(let i of responseJSON.results)
+        {
+            var link = document.createElement("A");
+            var node = document.createTextNode(i.name);
+            link.setAttribute("href", "https://www.w3schools.com");
+            link.appendChild(node);
+            var element = document.getElementById("individual_result");
+            element.appendChild(link);
+        }
+        // alert(responseJSON.name);
         
     }
     var superHeroName=document.getElementById("superHeroName").value;
-    xhrRequest.open('get',"https://superheroapi.com/api/4630807856945450/search/"+superHeroName,false);
+    xhrRequest.open('get',"https://superheroapi.com/api/4630807856945450/search/"+superHeroName,true);
     xhrRequest.send();
+    return false;
     
 }
 
