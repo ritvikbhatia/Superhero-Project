b6ea6340f1b5b81d9c5983ac75dd22744a5010f2
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-31 15:30:12 +0530

    handled nothing found

--- script/script.js
+++ script/script.js
@@ -8,11 +8,20 @@ function myFunction() {
     {
         console.log(xhrRequest.response);
         var responseJSON=JSON.parse(xhrRequest.response);
+        if(responseJSON.response=="error")
+        {
+            var link = document.createElement("p");
+            var node = document.createTextNode("Nothing Found");
+            link.appendChild(node);
+            var element = document.getElementById("individual_result");
+            element.appendChild(link);
+            return;
+        }
         for(let i of responseJSON.results)
         {
             var link = document.createElement("A");
             var node = document.createTextNode(i.name);
-            link.setAttribute("href", "https://www.w3schools.com");
+            link.setAttribute("href", "profile.html");
             link.appendChild(node);
             var element = document.getElementById("individual_result");
             element.appendChild(link);
